b5fd73af807a9e8bd1fba2fed8abf8eb78fa4c65
rust-in <huoxiaoliangchn@163.com>
2017-09-19 20:16:10 +0800

    finish E/H/I exercises for 2017's algorithm warm-up

--- OJ4TH-BUAA/2016/warm-up/E.cpp
+++ OJ4TH-BUAA/2016/warm-up/E.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <climits>
 using namespace std;
 
 /**
@@ -7,29 +8,57 @@ using namespace std;
 
 long fullNumber[10005];
 long tmp[10005];
+long res;
 
 void Merge(int left, int mid, int right)
 {
-    int lenl=mid-left-1;
-    int lenr=right-len;
+    int lenl=mid-left+1;
+    int lenr=right-mid;
 
-    long arrl[lenl], arrr[lenr];
+    long arrl[lenl+1], arrr[lenr+1];
     for(int i=0;i<lenl;i++)
         arrl[i]=tmp[left+i];
     for(int i=0;i<lenr;i++)
         arrr[i]=tmp[mid+i+1];
+    arrl[lenl]=arrr[lenr]=LONG_MAX;
+    int l=0,r=0;
+    for(int i=left;i<=right;i++){
+        if(arrl[l]<=arrr[r])
+            tmp[i]=arrl[l++];
+        else{
+            tmp[i]=arrr[r++];
+            res+=lenl-l;
+        }
+    }
+}
+
+void mergeSort(int left, int right)
+{
+    if(left<right){
+        int mid=(left+right)/2;
+        mergeSort(left, mid);
+        mergeSort(mid+1, right);
+        Merge(left, mid, right);
+    }
 }
 int main()
 {
     int len;
     while(~scanf("%d", &len)){
         for(int i=0;i<len;i++)
-            scanf("%d", &fullNumber[i]);
+            scanf("%ld", &fullNumber[i]);
 
         int cnt;
         scanf("%d", &cnt);
         while(cnt--){
-
+            int one,two;
+            scanf("%d %d", &one, &two);
+            int j=0;
+            res=0;
+            for(int i=one;i<=two;i++)
+                tmp[j++]=fullNumber[i];
+            mergeSort(0,j-1);
+            printf("%ld\n", res);
         }
     }
 }
---
+++ OJ4TH-BUAA/2016/warm-up/H.cpp
@@ -0,0 +1,38 @@
+#include <queue>
+#include <iostream>
+using namespace std;
+
+/**
+优先队列的使用
+*/
+
+struct cmp{
+    bool operator()(int &a, int &b){
+        return a>b; //小的优先级高
+    }
+};
+
+int main()
+{
+    priority_queue<int,vector<int>,cmp> que;
+    int len;
+    while(cin>>len){
+        for(int i=0;i<len;i++){
+            int tmp;
+            cin>>tmp;
+            que.push(tmp);
+        }
+        long res=0;
+        while(!que.empty()){
+            int a=que.top();
+            que.pop();
+            if(que.empty())
+                break;
+            int b=que.top();
+            que.pop();
+            res+=(a+b);
+            que.push(a+b);
+        }
+        cout << res << endl;
+    }
+}
