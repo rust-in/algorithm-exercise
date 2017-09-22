c1081c5919387fe2cc0ca5c2e17f9750bdca3678
rust-in <huoxiaoliangchn@163.com>
2017-09-22 21:20:30 +0800

    upload 2014's algorithm exercies.

---
+++ OJ4TH-BUAA/extra/2014/1A.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+/**
+¼òµ¥µÄººÅµËþ¸´Ï°
+*/
+
+void han(char a,char b,char c, int n)
+{
+    if(n==1)
+        cout << a << " to " << c << endl;
+    else{
+        han(a,c,b,n-1);
+        cout << a << " to " << c << endl;
+        han(b,a,c,n-1);
+    }
+}
+
+int main()
+{
+    int n;
+    while(cin>>n)
+    {
+        han('A','B','C',n);
+        cout << endl;
+    }
+}
