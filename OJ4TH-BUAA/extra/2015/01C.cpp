473093adf0b446fd126d71aefbe70bdd4b192197
rust-in <huoxiaoliangchn@163.com>
2017-10-12 21:25:40 +0800

    finish a problem in 2015's algorithm test

--- OJ4TH-BUAA/extra/2015/01C.cpp
+++ OJ4TH-BUAA/extra/2015/01C.cpp
@@ -1,7 +1,25 @@
 #include <cstdio>
 using namespace std;
 
+int num[1000005];
+
 int main()
 {
+    int len,k,l,r,tmp;
 
+    while(~scanf("%d",&len)){
+        scanf("%d",&num[0]);
+
+        for(int i=1;i<len;i++){
+            scanf("%d",&tmp);
+            num[i] = tmp ^ num[i-1];
+        }
+
+        scanf("%d",&k);
+        while(k--){
+            scanf("%d%d",&l,&r);
+            if(l>r) l=l+r-(r=l);
+            printf("%d\n",num[l-2]^num[r-1]);
+        }
+    }
 }
