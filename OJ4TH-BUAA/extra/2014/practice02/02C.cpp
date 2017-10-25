41d81b2b08de2786f94c6f2558234da8c77879b7
rust-in <huoxiaoliangchn@163.com>
2017-10-26 07:04:25 +0800

    fimish problem C
    Signed-off-by: rust-in <huoxiaoliangchn@163.com>

---
+++ OJ4TH-BUAA/extra/2014/practice02/02C.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+#define MAX 0xFFFFFF
+int pot[505][505];
+
+int main()
+{
+    int len,time;
+    while(cin>>len>>time){
+        for(int i=0;i<len;i++){
+            for(int j=0;j<len;j++){
+                cin >> pot[i][j];
+                if(pot[i][j]==-1)
+                    pot[i][j]=MAX;
+            }
+        }
+
+        for(int i=0;i<len;i++)
+            for(int j=0;j<len;j++)
+                for(int k=0;k<len;k++)
+                    pot[j][k]=min(pot[j][k],pot[j][i]+pot[i][k]);
+
+        int one,two;
+        for(int i=0;i<time;i++){
+            cin >> one >> two;
+            if(pot[one-1][two-1]==MAX || one == two)
+                cout << "jujue\n";
+            else
+                cout << pot[one-1][two-1] <<endl;
+        }
+    }
+}
